90256b2911b2d61bd93a6803083fc1c11a7d2f55
Alexandra <alexandra.uefa@gmail.com>
2017-09-14 00:31:22 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -7,14 +7,23 @@ import Hello from './components/Hello'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
+//we want to apply VueResource and VueRouter
+//to our Vue instance
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
-Vue.config.productionTip = false
+const router = new VueRouter()
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+//pointing routes to the components they should use
+router.map({
+  '/hello': {
+    component: Hello
+  }
 })
+
+//any invalid route will redirect to home
+router.redirect({
+  '*': '/hello'
+})
+
+router.start(App, '#app')
