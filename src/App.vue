5addd7ec72d1fcc54d75d20ecbf4f3cc8a0823d0
Alexandra <alexandra.uefa@gmail.com>
2017-09-13 23:33:31 +0200

    update app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+    <div id="wrapper">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <i class="glyphicon glyphicon-time"></i>
+                    Vue Time Tracker
+                </a>
+                <ul class="nav navbar-nav">
+                    <li><a v-link="'/home'">Home</a></li>
+                    <li><a v-link="'/time-entries'">Time Entries</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="col-sm-3">
+                
+            </div>
+            <div class="col-sm-9">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
 
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+    name: 'app',
+    components: {
+        Hello
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
