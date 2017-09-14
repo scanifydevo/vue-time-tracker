655f8c7ffa90633c85f24b8f45a78bdeeb1b2df0
Alexandra <alexandra.uefa@gmail.com>
2017-09-14 09:01:33 +0200

    update timeentries file

--- src/components/TimeEntries.vue
+++ src/components/TimeEntries.vue
@@ -29,6 +29,28 @@
                                 </strong>
                             </p>
                         </div>
+
+                        <div class="col-sm-2 text-center time-block">
+                            <h3 class="list-group-item-text total-time">
+                                <i class="glyphicon glyphicon-time"></i>
+                                {{ timeEntry.totalTime }}
+                            </h3>
+                            <p class="label label-primary text-center">
+                                <i class="glyphicon glyphicon-calendar"></i>
+                                {{ timeEntry.date }}
+                            </p>
+                        </div>
+
+                        <div class="col-sm-7 comment-section">
+                            <p>{{ timeEntry.comment }}</p>
+                        </div>
+
+                        <div class="col-sm-1">
+                            <button class="btn btn-xs btn-danger delete-button"
+                                @click="deleteTimeEntry(timeEntry)">
+                                X
+                            </button>
+                        </div>
                     </div>
                 </a>
             </div>
