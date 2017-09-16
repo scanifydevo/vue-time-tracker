e2b0be818b54de0ae748fa9cb84de389cf4b508e
Alexandra <alexandra.uefa@gmail.com>
2017-09-16 18:19:46 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Hello from './components/Hello'
+import Home from './components/Home.vue'
+import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
 //we want to apply VueResource and VueRouter
-//to our Vue instance
+//to our Vue instance 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
@@ -16,14 +18,22 @@ const router = new VueRouter()
 
 //pointing routes to the components they should use
 router.map({
-  '/hello': {
-    component: Hello
+  '/home': {
+    component: Home
+  },
+  '/time-entries': {
+    component: TimeEntries,
+    subRoutes: {
+      '/log-time': {
+        component: LogTime
+      }
+    }
   }
 })
 
 //any invalid route will redirect to home
 router.redirect({
-  '*': '/hello'
+  '*': '/home'
 })
 
 router.start(App, '#app')
